fe6870298afd7ad9f5086a1f925c2beec12a2208
Kurt Strecker <ksphotopro@gmail.com>
2021-06-30 22:58:41 -0400

    blinky example and spi + i2c pin assignments to readme

--- README.md
+++ README.md
@@ -4,3 +4,35 @@ A base starting point for projects using the STM B-L475E-IOT01A Iot Node dev boa
 
 # useful links
 https://github.com/stm32-rs/stm32-rs
+
+
+## I2C Addresses
+|Modules |Description |SAD[0:6]+R/W |I2C write address |I2C read address
+--- | --- | --- | --- | ---
+|HTS221 |Capacitive digital sensor for relativehumidity and temperatur |1011111x |0xBE |0xBF
+|LIS3MDL |3-axis magnetometer |0011110x |0x3C |0x3D
+|LPS22HB |MEMS nano pressure sensor |1011101x |0xBA |0xBB
+|LSM6DSL |3D accelerometer and3D gyroscope |1101010x |0xD4 |0xD5
+|VL53L0X |Time-of-Flight ranging and gesturedetection sensor |0101001x |0x52 |0x53
+|ST25DV04K |Dynamic NFC/RFID Tag IC |1010x11x |0xAE for systemarea0xA6 for usermemory |0xAF for systemarea0xA7 for usermemory
+|STSAFE-A110 |Highly secure solution |0100000x |0x40 |0x41
+
+
+## SPI CSN Assignments
+|Module |Description |Pin Name
+--- | --- | ---
+|ISM43362-SPI3_CSN |WiFi |PE0
+|SPSGRF-915-SPI3_CS |Sub Ghz Module |PB5
+|SPBTLE-RF-SPI3_CS |Bluetooth LE |PD13
+|SPI3_SCK |MCU SCK |PC10
+|SPI3_MISO |MCU MISO |PC11
+|SPI3_MOSI |MCU MOSI |PC12
+
+## Module Reset Pins
+|Module |Pin Name
+--- | ---
+|SPBTLE-RF-RST |PA8
+|ISM43362-RST |PE8
+|STSAFE-A110-RESET |PD7
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-    println!("Hello, world!");
+#![deny(unsafe_code)]
+#![no_main]
+#![no_std]
+
+
+use panic_halt as _;
+use cortex_m_rt::entry;
+use stm32l4xx_hal as hal;
+use hal::prelude::*;
+use hal::delay::Delay;
+
+#[entry]
+fn main() -> ! {
+    let cp = cortex_m::Peripherals::take().unwrap();
+    let dp = hal::stm32::Peripherals::take().unwrap();
+
+    let mut flash = dp.FLASH.constrain(); // .constrain();
+    let mut rcc = dp.RCC.constrain();
+    let mut pwr = dp.PWR.constrain(&mut rcc.apb1r1);
+
+    let clocks = rcc.cfgr.hclk(8.mhz()).freeze(&mut flash.acr, &mut pwr);
+
+    let mut gpiob = dp.GPIOB.split(&mut rcc.ahb2);
+    let mut led1 = gpiob
+        .pb14
+        .into_push_pull_output(&mut gpiob.moder, &mut gpiob.otyper);
+
+    let mut gpioa = dp.GPIOA.split(&mut rcc.ahb2);
+    let mut led2 = gpioa.pa5.into_push_pull_output(&mut gpioa.moder, &mut gpioa.otyper);
+
+    let mut timer = Delay::new(cp.SYST, clocks);
+
+    loop {
+        // block!(timer.wait()).unwrap();
+        timer.delay_ms(1000_u32);
+        led2.set_low().ok();
+        led1.set_high().ok();
+        // block!(timer.wait()).unwrap();
+        timer.delay_ms(1000_u32);
+        led1.set_low().ok();
+        led2.set_high().ok();
+    }
 }
